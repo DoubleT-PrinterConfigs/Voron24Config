cd30798ee2622a4842a6436a66f2a33c7725c94e
thiagolocatelli <thiago.locatelli@gmail.com>
2023-03-01 03:03:36 +0000

    Updated klicky to new macros

--- NonConfigFiles/FlashMcus/flashboards.sh
+++ NonConfigFiles/FlashMcus/flashboards.sh
@@ -21,23 +21,30 @@ echo ----------------------------------------------------------
 echo
 
 ### Flash the Spider v2.2 via USB...
-echo "Start processing for the Spider v2.2..."
+echo "Start processing for the Spider 2.2..."
 make clean KCONFIG_CONFIG=config.spider22
 make menuconfig KCONFIG_CONFIG=config.spider22
 make -j4 KCONFIG_CONFIG=config.spider22
-make flash KCONFIG_CONFIG=config.spider22 FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_440025001450325635393320-if00
-
-# Sometimes the Spider22 is stuck in DFU mode...
+make flash KCONFIG_CONFIG=config.spider22 FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_35004B001951373434353332-if00
+# Sometimes the Octopus is stuck in DFU mode...
 if [ $? -eq 0 ]; then
    echo "*** Successfully flashed Spider v2.2"
 else
    echo "*** Unable to flash via serial path, attempting via USB device ID..."
-   make flash KCONFIG_CONFIG=config.octopus FLASH_DEVICE=0483:df11
+   make flash KCONFIG_CONFIG=config.spider22 FLASH_DEVICE=0483:df11
 fi
 
 echo
 echo ----------------------------------------------------------
 echo
+
+### Flash the SB2040 via CAN Boot...
+#echo "Start processing for the Fly-sht36..."
+make clean KCONFIG_CONFIG=config.sht36v2.7a268842af31
+make menuconfig KCONFIG_CONFIG=config.sht36v2.7a268842af31
+make -j4 KCONFIG_CONFIG=config.sht36v2.7a268842af31
+python3 ~/CanBoot/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u 7a268842af31
+
 
 ### Flash the SB2040 via CAN Boot...
 #echo "Start processing for the SB2040..."
